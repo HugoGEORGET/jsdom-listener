6c659778f398b5b055c6615b35b50b4a38c74247
Hugo Georget <hgeorget@workit-software.com>
2020-12-17 14:59:57 +0100

    Dump reponses' body in console

--- CustomResourceLoader.js
+++ CustomResourceLoader.js
@@ -4,13 +4,15 @@ class CustomResourceLoader extends jsdom.ResourceLoader {
   fetch(url, options) {
     if (options.element) {
       console.log(
-        `Element ${options.element.localName} is requesting the url ${url}\n`
+        `Element ${options.element.localName} is requesting the url ${url}`
       );
     } else {
-      console.log(`URL called: ${url}\n`);
+      console.log(`URL called: ${url}`);
     }
 
-    return super.fetch(url, options);
+    return super
+      .fetch(url, options)
+      .then((res) => console.log(`Response: ${res}\n`));
   }
 }
 
